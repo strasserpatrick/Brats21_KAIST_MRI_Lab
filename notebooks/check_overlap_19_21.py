9d1290f2360e8822426f030bb6d04011da40b240
Patrick Strasser <patrick.strasser@stud-mail.uni-wuerzburg.de>
2024-04-18 11:31:56 +0000

    feat: refine notebooks, add 19-21 comparison

--- notebooks/check_overlap_19_21.py
+++ notebooks/check_overlap_19_21.py
@@ -6,11 +6,12 @@ from typing import List
 import concurrent.futures
 
 # %%
+
 brats2021_label_path: Path = Path(
     "/home/stud/strasser/data/nnUNet_raw_data/Task500_BraTS2021/labelsTr"
 )
-brats2018_train_path: Path = Path(
-    "/home/stud/strasser/archive/brats2018/MICCAI_BraTS_2018_Data_Training"
+brats2019_train_path: Path = Path(
+    "/home/stud/strasser/archive/brats2019/MICCAI_BraTS_2019_Data_Training"
 )
 
 # validation data holds no segmentation labels
@@ -21,34 +22,34 @@ brats2021_label_nii_gz_file_paths: List[Path] = list(brats2021_label_path.glob("
 brats2021_label_nii_gz_file_paths[:10]
 
 # %%
-brats_2018_hgg = brats2018_train_path / "HGG"
-brats_2018_lgg = brats2018_train_path / "LGG"
+brats_2019_hgg = brats2019_train_path / "HGG"
+brats_2019_lgg = brats2019_train_path / "LGG"
 
 hgg_paths = [
     folder
-    for instance in brats_2018_hgg.iterdir()
+    for instance in brats_2019_hgg.iterdir()
     if instance.is_dir()
     for folder in list(instance.glob("*_seg.nii"))
 ]
 lgg_paths = [
     folder
-    for instance in brats_2018_lgg.iterdir()
+    for instance in brats_2019_lgg.iterdir()
     if instance.is_dir()
     for folder in list(instance.glob("*_seg.nii"))
 ]
 
-brats2018_segmentation_mask_file_paths: List[Path] = hgg_paths + lgg_paths
-brats2018_segmentation_mask_file_paths[:10]
+brats2019_segmentation_mask_file_paths: List[Path] = hgg_paths + lgg_paths
+brats2019_segmentation_mask_file_paths[:10]
 
 # %%
-# now check for one file of 2018, if its segmentation mask occurs in 2021
+# now check for one file of 2019, if its segmentation mask occurs in 2021
 
-brats2018_segmentation_path = brats2018_segmentation_mask_file_paths[10]
-brats2018_segmentation_image = nib.load(brats2018_segmentation_path)
+brats2019_segmentation_path = brats2019_segmentation_mask_file_paths[10]
+brats2019_segmentation_image = nib.load(brats2019_segmentation_path)
 
 # Numpy-Array aus der .nii-Datei extrahieren
-brats2018_segmentation_np = brats2018_segmentation_image.get_fdata()
-brats2018_segmentation_np.shape
+brats2019_segmentation_np = brats2019_segmentation_image.get_fdata()
+brats2019_segmentation_np.shape
 
 # %%
 # # Naive without multitasking
@@ -67,22 +68,23 @@ brats2018_segmentation_np.shape
 # print("no overlap")
 
 # %%
-print("brats 2018: ", len(brats2018_segmentation_mask_file_paths))
+print("brats 2019: ", len(brats2019_segmentation_mask_file_paths))
 print("brats 2021: ", len(brats2021_label_nii_gz_file_paths))
 
 print(
     "product: ",
-    len(brats2018_segmentation_mask_file_paths)
+    len(brats2019_segmentation_mask_file_paths)
     * len(brats2021_label_nii_gz_file_paths),
 )
 
+
 # %%
-def check_segmentation_equality(brats2018_fp, brats2021_nii_gz_path):
-    brats2018_segmentation_nparr = nib.load(brats2018_fp).get_fdata()
+def check_segmentation_equality(brats2019_fp, brats2021_nii_gz_path):
+    brats2019_segmentation_nparr = nib.load(brats2019_fp).get_fdata()
     brats2021_segmentation_image = nib.load(brats2021_nii_gz_path)
     brats2021_segmentation_np = brats2021_segmentation_image.get_fdata()
 
-    if np.array_equal(brats2018_segmentation_nparr, brats2021_segmentation_np):
+    if np.array_equal(brats2019_segmentation_nparr, brats2021_segmentation_np):
         raise ValueError(
             f"Duplicate in segmentation mask for file {brats2021_nii_gz_path}"
         )
@@ -94,12 +96,12 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
     futures = []
     total = 0
 
-    for brats2018_fp in brats2018_segmentation_mask_file_paths:
+    for brats2019_fp in brats2019_segmentation_mask_file_paths:
         for brats2021_nii_gz_path in brats2021_label_nii_gz_file_paths:
             total += 1
             futures.append(
                 executor.submit(
-                    check_segmentation_equality, brats2018_fp, brats2021_nii_gz_path
+                    check_segmentation_equality, brats2019_fp, brats2021_nii_gz_path
                 )
             )
 
@@ -111,5 +113,3 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
         print(f"checked {done} / {total}", end="\r")
 
 print("no overlap")
-
-
